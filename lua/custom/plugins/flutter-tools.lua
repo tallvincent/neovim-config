50f8b1680e2ba38a293428fc955454d38d367f84
Vincent Talldira <vincent@tallvincent.com>
2024-12-23 17:46:20 +1030

    Better flutter setup with keymaps.

--- lua/custom/plugins/flutter-tools.lua
+++ lua/custom/plugins/flutter-tools.lua
@@ -6,6 +6,18 @@ return {
     -- 'stevearc/dressing.nvim', -- optional for vim.ui.select
   },
   config = function()
-    require('flutter-tools').setup {}
+    require('flutter-tools').setup {
+      lsp = {
+        color = {
+          enabled = true,
+        },
+        on_attach = function()
+          local set = vim.keymap.set
+          set('n', '<leader>fR', '<cmd>FlutterRun<CR>', { desc = '[F]lutter [R]un' })
+          set('n', '<leader>fr', '<cmd>FlutterRestart<CR>', { desc = '[F]lutter [R]estart' })
+          set('n', '<leader>fS', '<cmd>FlutterQuit<CR>', { desc = '[F]lutter [Q]uit' })
+        end,
+      },
+    }
   end,
 }
